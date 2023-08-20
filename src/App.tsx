82b9af0e2d5f6ab4b4df54c58aade5ea95a3e26c
Alex Liew <liewtwoo42@gmail.com>
2023-08-20 14:52:17 -0400

    fetch data from pokemon api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { PokemonIndex } from "./components/PokemonIndex"
-
+import { useEffect } from "react"
 function App() {
+
+  useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon/charizard"
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData();
+  }, [])
+
+
+
   return (
     <div>
       <PokemonIndex />
-      hello
     </div>
   )
 }
